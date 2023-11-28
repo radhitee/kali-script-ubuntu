48b78c777e02830ca3d104a1581c7babf4a42be5
radhitee <mr.radhite@gmail.com>
2023-11-28 11:10:28 +0700

    Update start_scan.sh

--- start_scan.sh
+++ start_scan.sh
@@ -14,9 +14,6 @@ cd "$project_name" || exit
 # Meminta domain yang ingin di-scan
 read -p "Input domain to scan: " domain_name
 
-# Menanyakan apakah perlu parameter untuk Nuclei
-read -p "Need parameter for nuclei? (y/n): " need_param
-
 if [ "$need_param" == "y" ]; then
     read -p "Input your nuclei param: " nuclei_param
 fi
@@ -29,12 +26,22 @@ echo "Running http-toolkit..."
 # Menjalankan httpx-toolkit untuk mengecek subdomain yang aktif
 httpx-toolkit -l subdomains.txt -o active_domains.txt
 
-# Menjalankan Nuclei untuk melakukan scanning
-echo "Running nuclei..."
-if [ "$need_param" == "y" ]; then
-    nuclei -l active_domains.txt -o nuclei_report.txt "$nuclei_param"
-    #subfinder -d "$domain_name" -o subdomains.txt | httpx-toolkit -o active_domains.txt | nuclei "$nuclei_param" -o nuclei_report.txt
+# Menanyakan apakah pengguna ingin menjalankan Nuclei
+read -p "Do you want to run Nuclei? (y/n): " run_nuclei
+
+if [ "$run_nuclei" == "y" ]; then
+    echo "Running nuclei..."
+    
+    # Menanyakan apakah perlu parameter untuk Nuclei
+    read -p "Need parameter for nuclei? (y/n): " need_param
+    
+    if [ "$need_param" == "y" ]; then
+        nuclei -l active_domains.txt -o nuclei_report.txt "$nuclei_param"
+        #subfinder -d "$domain_name" -o subdomains.txt | httpx-toolkit -o active_domains.txt | nuclei "$nuclei_param" -o nuclei_report.txt
+    else
+        nuclei -l active_domains.txt -o nuclei_report.txt
+        #subfinder -d "$domain_name" -o subdomains.txt | httpx-toolkit -o active_domains.txt | nuclei -o nuclei_report.txt
+    fi
 else
-    nuclei -l active_domains.txt -o nuclei_report.txt
-    #subfinder -d "$domain_name" -o subdomains.txt | httpx-toolkit -o active_domains.txt | nuclei -o nuclei_report.txt
+    echo "Skipping Nuclei."
 fi
